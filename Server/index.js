a59a188f2cdbeff8a837f44fbc14f3b88a31affe
ShikiWeiss <shiki56@gmail.com>
2021-02-16 12:36:04 +0200

    server creation

---
+++ Server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const URLS = require("./settings/URLS")
+const bodyParser = require("body-parser")
+
+
+app.use(cors());
+app.listen(URLS.serverPort, () => {
+    console.log(`Fakelook server is running at ${URLS.serverDomain}:${URLS.serverPort}`);
+})
+
+app.use(bodyParser.json());
