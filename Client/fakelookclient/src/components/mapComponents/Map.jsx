41ab957944e90f81349a71f800273342fd39a89b
yamy466 <almog.elisha@gmail.com>
2021-02-23 21:28:16 +0200

    google maps without key

--- Client/fakelookclient/src/components/mapComponents/Map.jsx
+++ Client/fakelookclient/src/components/mapComponents/Map.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Map } from "google-maps-react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const mapStyles = {
   width: "100%",
