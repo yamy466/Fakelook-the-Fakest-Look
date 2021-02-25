fdc09862dff1ba91117f537f2a2f039ea97411cf
yamy466 <almog.elisha@gmail.com>
2021-02-25 13:00:22 +0200

    routes fixed?

---
+++ Client/fakelookclient/src/components/routing/routes.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Home from "../homeComponents/home";
+import FakeLookMap from "../mapComponents/map.jsx";
+import AboutUs from "../aboutUsComponents/aboutUs";
+import { Switch, Route } from "react-router-dom";
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/map" component={FakeLookMap} />
+          <Route exact path="/aboutus" component={AboutUs} />
+        </Switch>
+      </div>
+    );
+  }
+}
