07d3ebe04a7a030a01ba29571f6f4bc48b42ebfa
yamy466 <almog.elisha@gmail.com>
2021-02-25 13:14:14 +0200

    fixed popup issue

--- Client/fakelookclient/src/components/mapComponents/CustomIcon.js
+++ Client/fakelookclient/src/components/mapComponents/CustomIcon.js
@@ -1,5 +1,5 @@
 import L from "leaflet";
-import fakelookLogo from "../../logo/logo_transparent.png"
+import fakelookLogo from "../../logo/logo_transparent.png";
 
 // class CustomIcon extends L.Icon {
 //   constructor(
@@ -16,17 +16,23 @@ import fakelookLogo from "../../logo/logo_transparent.png"
 //   }
 // }
 
-const customIcon = (iconUrl = fakelookLogo,iconWidth = 70,iconHeight = 70,className = "") => {
+const customIcon = (
+  iconUrl = fakelookLogo,
+  iconWidth = 70,
+  iconHeight = 70,
+  className = ""
+) => {
   return new L.Icon({
-  iconUrl: iconUrl,
-  iconRetinaUrl: null,
-  iconAnchor: null,
-  popupAnchor: null,
-  shadowUrl: null,
-  shadowSize: null,
-  shadowAnchor: null,
-  iconSize: new L.Point(iconWidth, iconHeight),
-  className: className,
-})};
+    iconUrl: iconUrl,
+    iconRetinaUrl: null,
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(iconWidth, iconHeight),
+    className: className,
+  });
+};
 
 export default customIcon;
